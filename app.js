d2e9b68ac7da200e4bb9c94958a13ef6964cece7
ivan2798 <inge1998@hotmail.com>
2022-09-19 11:18:44 -0500

    Strings

---
+++ app.js
@@ -0,0 +1,11 @@
+//cadena de texto aka Strings
+let nombre = "Jon";
+let apellido = "Wick";
+let saludo = new String("Hola mundo");
+let lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos architecto voluptates officiis distinctio quo ipsam voluptatum assumenda odio id. Exercitationem voluptatibus quae, maiores suscipit veniam dolor similique asperiores quos quis.";
+
+
+console.log(nombre,apellido,saludo);
+console.log(nombre.length,apellido.length,saludo.length);
+console.log(nombre.toUpperCase(),apellido.toLocaleLowerCase());
+console.log(lorem.includes("ipsum"),lorem.trim(),lorem.split(" "));
